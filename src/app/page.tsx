2f4bf18044773396b9ceed7d1eeb4ed186c8b193
sjsj.cloud <sjsj.cloud@gmail.com>
2025-04-12 10:21:03 +0530

    chore: cleanup unused components and update gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
-import NavBar from "@/components/navbar";
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
-      <NavBar />
-      <section className="w-screen h-[90vh] flex justify-center items-center">
-        <div className="w-screen  md:w-[500px] h-[300px] md:relative" >
-          <span className="headline corner TL block md:inline-block">YOUR</span>
-          <span className="headline corner  TR">PET&apos;s</span>
-          <br />
-          <span className="headline corner  BL">HAPPY</span>
-          <span className="headline corner  BR">PLACE</span>
-          <Image src={"./mascot.svg"} width={500} height={300} alt="Chill dog" />
-        </div>
-      </section>
+    <main className="min-h-screen bg-[#D7ECFA] flex flex-col items-center justify-center relative">
+      {/* Logo Section */}
+      <div className="w-full max-w-[800px] px-4">
+        <Image
+          src="/brand_assets/Word Mark/White (Primary)/MP_Wordmark_Primary.png"
+          alt="Masterpet Wordmark"
+          width={800}
+          height={200}
+          priority
+          className="w-full h-auto"
+        />
+      </div>
 
+      {/* Scroll Indicator */}
+      <div className="absolute bottom-12 animate-bounce">
+        <div className="flex flex-col items-center gap-1">
+          <div className="w-6 h-6 border-b-2 border-r-2 border-[#00008D] rotate-45"></div>
+          <div className="w-6 h-6 border-b-2 border-r-2 border-[#00008D] rotate-45 -mt-3"></div>
+          <div className="w-6 h-6 border-b-2 border-r-2 border-[#00008D] rotate-45 -mt-3"></div>
+        </div>
+      </div>
     </main>
-  );
+  )
 }
